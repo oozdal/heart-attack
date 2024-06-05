6fb36bd6bc3f649afeae377520233e4788610903
Ozer, Ozdal <ozerozdal@gmail.com>
2024-06-05 09:20:31 -0400

    updating frontend

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,14 +37,41 @@ st.markdown(dark, unsafe_allow_html=True)
 # Streamlit app
 st.subheader("Two-year Survival Predictor After Heart Attack")
 
-uploaded_file = st.file_uploader("Upload your echocardiogram.test to get your predictions.", label_visibility="visible")
+checkbox_val = st.radio("Do you have an echocardiogram.test file to upload and get your predictions?", ("Yes", "No"), index=1)
+if checkbox_val == "Yes":
+    uploaded_file = st.file_uploader("Upload your echocardiogram.test to get your predictions.", label_visibility="visible")
+else:
+    st.write("Please answer the following questions and then submit to get your prediction!")
 
+    # Age
+    age = st.slider('At what age did you have a heart attack?', 
+                    min_value=int(35), max_value=int(86), step=1, value=63)
+    
+    # Fractional Shortening
+    frac_short = st.slider('What is your fractional shortening? (A measure of contracility around the heart.)',
+                    min_value=0.01, max_value=0.61, value=0.216845)
 
-if st.button("Submit"):
+    # epss
+    epss = st.slider('What is your epss value? (E-point septal separation is another measure of contractility.)', 
+                    min_value=0.0, max_value=40.0, value=12.446646)
+    
+    # lvdd
+    lvdd = st.slider('What is your lvdd value? (Left ventricular end-diastolic dimension. This is a measure of the size of the heart at end-diastole.)', 
+                    min_value=3.10, max_value=6.78, value=4.77)
 
-    # Validate inputs
-    try:
-        if uploaded_file is not None:
+    # wall motion index
+    wall_motion_index = st.slider('What is your wall motion index? (Equals to wall-motion-score divided by number of segments seen.)',
+                    min_value=1.00, max_value=3.00, value=1.398541)
+
+    # pericardial effusion
+    pericardial_effusion = st.radio("Do you have pericardial effusion? (Pericardial effusion is fluid around the heart.)", 
+                    ("Yes", "No"), index=1)
+
+if checkbox_val == "Yes" and uploaded_file is not None:
+    if st.button("Submit"):
+
+        # Validate inputs
+        try:
             # Save uploaded file temporarily to disk, load and split the file into pages, delete temp file
             with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
                 tmp_file.write(uploaded_file.read())
@@ -159,34 +186,36 @@ if st.button("Submit"):
             # Display an image of a guy having an heart attack
             st.image('model/heart_attack.png', caption='Heart Attack')
 
-        else:
-            # Read the data file into a DataFrame
-            df = pd.read_csv("data/echocardiogram_predictions.csv") 
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
 
-            # Display the DataFrame
-            st.dataframe(df)
+elif checkbox_val == "No":
+    if st.button("Submit"):
 
-            # Display the Plotly Confusion Matrix
-            html_data = read_from_json('model/RandomForest_CM.json')
-            st.plotly_chart(html_data, use_container_width=True, template='plotly')
+        # Read the data file into a DataFrame
+        df = pd.read_csv("data/echocardiogram_predictions.csv") 
 
-            # Display Discrimination Threshold - Random Forest
-            #html_data = read_from_json('model/rf_disc_threshold.json')
-            #st.plotly_chart(html_data, use_container_width=True, template='plotly')
+        # Display the DataFrame
+        st.dataframe(df)
 
-            # Display Discrimination Threshold - Random Forest - Alternative
-            HtmlFile = open('model/rf_disc_threshold.html', 'r', encoding='utf-8')
-            source_code = HtmlFile.read() 
-            components.html(source_code, height = 600, width=1000)
+        # Display the Plotly Confusion Matrix
+        html_data = read_from_json('model/RandomForest_CM.json')
+        st.plotly_chart(html_data, use_container_width=True, template='plotly')
 
-            # Display a ROC-AUC comparison plot 
-            HtmlFile = open('model/roc_auc_comparison.html', 'r', encoding='utf-8')
-            source_code = HtmlFile.read() 
-            components.html(source_code, height = 800, width=1000)
-    
-            # Display an image of a guy having an heart attack
-            st.image('model/heart_attack.png', caption='Heart Attack')
+        # Display Discrimination Threshold - Random Forest
+        #html_data = read_from_json('model/rf_disc_threshold.json')
+        #st.plotly_chart(html_data, use_container_width=True, template='plotly')
 
-    except Exception as e:
-        st.error(f"An error occurred: {e}")
+        # Display Discrimination Threshold - Random Forest - Alternative
+        HtmlFile = open('model/rf_disc_threshold.html', 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 600, width=1000)
+
+        # Display a ROC-AUC comparison plot 
+        HtmlFile = open('model/roc_auc_comparison.html', 'r', encoding='utf-8')
+        source_code = HtmlFile.read() 
+        components.html(source_code, height = 800, width=1000)
+
+        # Display an image of a guy having an heart attack
+        st.image('model/heart_attack.png', caption='Heart Attack')
 
